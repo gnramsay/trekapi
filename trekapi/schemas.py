961e2b577de96ab6114c497c201f868203f66e41
Grant Ramsay <grant@gnramsay.com>
2022-09-25 15:37:20 +0100

    add examples to the Series Schema
    Signed-off-by: Grant Ramsay <grant@gnramsay.com>

--- trekapi/schemas.py
+++ trekapi/schemas.py
@@ -18,3 +18,25 @@ class Series(BaseModel):
         """Set this as ORM compatible."""
 
         orm_mode = True
+        schema_extra = {
+            "example": {
+                "series_number": 4,
+                "name": "Deep Space Nine",
+                "url": (
+                    "https://en.wikipedia.org/wiki/Star_Trek"
+                    ":_Deep_Space_Nine"
+                ),
+                "season_count": "7",
+                "episode_count": "176",
+                "episodes_url": (
+                    "https://en.wikipedia.org/wiki/List_of_Star_Trek"
+                    ":_Deep_Space_Nine_episodes"
+                ),
+                "dates": "January 4, 1993 - May 31, 1999",
+                "logo": (
+                    "https://upload.wikimedia.org/wikipedia/commons/thumb"
+                    "/e/e7/Star_Trek_DS9_logo.svg/"
+                    "220px-Star_Trek_DS9_logo.svg.png"
+                ),
+            }
+        }
