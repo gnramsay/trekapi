e96d833297c890cf8bdf414e56d6d9861e5d7298
Grant Ramsay <grant@gnramsay.com>
2022-09-25 11:31:58 +0100

    enable SQLAlchemy
    Signed-off-by: Grant Ramsay <grant@gnramsay.com>

--- trekapi/main.py
+++ trekapi/main.py
@@ -2,15 +2,29 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 
+from trekapi import models
 from trekapi.api import trek_api
+from trekapi.database import engine
 from trekapi.views import home
 
 api = FastAPI(
     title="TrekAPI",
     description="An API to access Star Trek(tm) Episode data.",
     version="0.1.0",
+    license_info={
+        "name": "MIT",
+        "url": "https://opensource.org/licenses/MIT",
+    },
+    redoc_url=None,
+    contact={
+        "Developer name": "Grant Ramsay",
+        "website": "https://www.gnramsay.com",
+        "email": "grant@gnramsay.com",
+    },
 )
 
 api.mount("/static", StaticFiles(directory="static"), name="static")
 api.include_router(home.router)
 api.include_router(trek_api.router)
+
+models.Base.metadata.create_all(engine)
